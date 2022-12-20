b522968dc104516556d3a3cbdc0f1125b15ce7f1
Aditya Deshpande <54365703+aditya1331@users.noreply.github.com>
2022-12-20 16:23:24 +0530

    Animating the background using tween

--- lib/Pages/Home_page.dart
+++ lib/Pages/Home_page.dart
@@ -9,6 +9,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 double _buttonRadius = 100;
+final Tween<double> _backgroundScale = Tween<double>(begin: 0.0,end: 1.0);
 
   @override
   Widget build(BuildContext context) {
@@ -26,8 +27,18 @@ double _buttonRadius = 100;
   }
   Widget _pageBackground()
   {
-    return Container(
-      color: Colors.blue,
+    return TweenAnimationBuilder(
+      tween: _backgroundScale,
+      curve: Curves.easeOutQuad,
+      duration: Duration(seconds: 2),
+      builder: (_context,double _scale,child){
+        return Transform.scale(scale: _scale,
+          child: child,);
+
+      },
+      child: Container(
+        color: Colors.blue,
+      ),
     );
   }
   Widget _CircularANimationButton()
@@ -41,8 +52,15 @@ double _buttonRadius = 100;
         },
         child: AnimatedContainer(
           duration: const Duration(seconds: 2),
+          curve: Curves.bounceInOut,
           height: _buttonRadius,
           width: _buttonRadius,
+          child: const Center(
+            child: Text(
+              "Basic",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
           decoration: BoxDecoration(color: Colors.purple,borderRadius: BorderRadius.circular(_buttonRadius)),
         ),
       ),
